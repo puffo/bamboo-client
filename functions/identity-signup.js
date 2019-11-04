8cf41a9674857926509aaa81a04326f5a5ebbd43
puffo <puffo@users.noreply.github.com>
2019-11-04 02:57:50 +0200

    :bug: Fix .env invocation and remote calls

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -1,24 +1,30 @@
-import fetch from "node-fetch";
+require("dotenv").config();
+const fetch = require("node-fetch");
 
 const { SERVER_HOST } = process.env;
 const USERS_ENDPOINT = SERVER_HOST + "users";
 
 exports.handler = async (event, context) => {
   const { identity, user } = context.clientContext;
+
+  const requestBody = JSON.stringify({
+    email: (user && user.email) || "blank",
+    mobileNumber: "unknown",
+    externalReference: (user && user.id) || "1231"
+  });
+
   console.log(user);
+  console.log(requestBody);
+  console.log(USERS_ENDPOINT);
   return fetch(USERS_ENDPOINT, {
     method: "POST",
     headers: { Accept: "application/json" },
-    body: JSON.stringify({
-      email: user.email,
-      mobileNumber: "unknown",
-      externalReference: user.id
-    })
+    body: requestBody
   })
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
-      body: data
+      body: JSON.stringify(data)
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
